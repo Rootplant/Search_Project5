25ed4fe2ffb05dd64e8137764444f84e96dfa565
Rootplant <ho7465@gmail.com>
2025-12-03 17:18:36 +0900

    Update StockServiceImpl.java

--- backend/src/main/java/com/boot/service/StockServiceImpl.java
+++ backend/src/main/java/com/boot/service/StockServiceImpl.java
@@ -3,6 +3,7 @@ package com.boot.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.boot.dao.StockInfoDAO;
 import com.boot.dao.StockMapper;
 import com.boot.dto.StockInfoDTO;
 import com.boot.dto.StockNewsDTO;
@@ -12,6 +13,9 @@ public class StockServiceImpl implements StockService {
 
     @Autowired
     private StockMapper mapper;
+
+    @Autowired
+    private StockInfoDAO stockDAO;
 
     @Override
     public void insertStockInfo(StockInfoDTO dto) {
